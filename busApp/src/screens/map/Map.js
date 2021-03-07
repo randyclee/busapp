1d21249de27ade7469cf956c1e52dd01de0c755c
Randy Lee <randylee@ibm.com>
2021-03-06 19:24:43 -0500

    added bus route feature

--- busApp/src/screens/map/Map.js
+++ busApp/src/screens/map/Map.js
@@ -1,44 +1,69 @@
 import React, { Component } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 import MapView from 'react-native-maps';
 import MapViewDirections from 'react-native-maps-directions';
 
 
-const origin = {latitude: 37.3318456, longitude: -122.0296002};
-const destination = {latitude: 37.771707, longitude: -122.4053769};
+const origin = "17 Mary Wilson Court";
+const destination = "73 Pathlane Avenue";
 const GOOGLE_MAPS_APIKEY = 'AIzaSyCQqXJflchicdlH2Iq0AMnh126dBaz86dg';
 
 
-class Map extends Component {
-  render() {
-    return (
-      <MapView style={styles.map} initialRegion={{
-          latitude:41.0082,
-          longitude:28.9784,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421
-        }}
-        style={StyleSheet.absoluteFill}>
 
-        <MapViewDirections
-          origin={origin}
-          destination={destination}
-          apikey={GOOGLE_MAPS_APIKEY}
-        />
-      </MapView>
-    );
-  }
+export default class Map extends Component {
+	state = {
+		longitude: 0,
+    latitude: 0
+	};
+
+	findCoordinates = () => {
+		navigator.geolocation.getCurrentPosition(
+			position => {
+				const longitude = JSON.stringify(new Number(position.coords.longitude));
+        const latitude = JSON.stringify(new Number(position.coords.latitude));
+				this.setState({ longitude, latitude });
+			},
+			error => Alert.alert(error.message),
+			{ enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+		);
+	};
+
+	render() {
+		return (
+      <View style={styles.container}>
+					<MapView style={styles.map} region={{
+		          latitude:43.651070,
+		          longitude:-79.347015,
+		          latitudeDelta: 1.5,
+		          longitudeDelta: 1.5
+		        }}
+						showsUserLocation={true}
+		        style={StyleSheet.absoluteFill}>
+
+		        <MapViewDirections
+		          origin={origin}
+		          destination={destination}
+		          apikey={GOOGLE_MAPS_APIKEY}
+							mode="TRANSIT"
+		        />
+		      </MapView>
+      </View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF'
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10
+	}
 });
-
-export default Map;
